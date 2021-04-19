5520193fc5fe306d23d6851ba06b73a7b796840c
CesarRomeroDev <cesar_romero.dev@outlook.com>
2021-04-18 21:04:25 -0500

    conversor_string

--- conversor.py
+++ conversor.py
@@ -1,7 +1,38 @@
-pesos = input("¿Cuantos pesos mexicanos tienes?: ")
-pesos = float(pesos)
-valor_dolar = 20.09
-dolares = pesos / valor_dolar
-dolares = round(dolares, 2)
-dolares = str(dolares)
-print("Tienes $" + dolares + " dolares")
+menu = """
+Bienvenido al conversor de monedas 💰💹💵💲
+
+1 - Pesos Colombianos
+2 - Pesos Argrntinos
+3 - Pesos Mexicanos
+
+introduce un numero: """
+
+opcion = int(input(menu))
+
+if opcion == 1:
+    pesos = input("¿Cuantos pesos colombianos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 3875
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dolares")
+elif opcion == 2:
+    pesos = input("¿Cuantos pesos argetinos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 65
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dolares")
+elif opcion == 3:
+    pesos = input("¿Cuantos pesos mexicanos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 19.93
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dolares")
+else:
+    print('ingresa una opción correcta por favor. ')
+
