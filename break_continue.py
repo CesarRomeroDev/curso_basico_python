9a4e83eb8be27e8ac132c92ddeddb9b509712396
CesarRomeroDev <cesar_romero.dev@outlook.com>
2021-05-05 00:25:57 -0500

    break_continue

---
+++ break_continue.py
@@ -0,0 +1,12 @@
+def ran():
+    
+    palabra = input('Escribe una palabra: ')
+    for i in palabra:
+        if i == 'o':
+            break
+        print(i)
+
+
+
+if __name__ == '__main__':
+    ran()
